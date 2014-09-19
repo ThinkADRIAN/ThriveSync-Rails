285bfcc22861c0720323258f3be175f283b38d53
Adrian Cunanan <adrian@adriancunanan.com>
2014-09-19 08:53:36 -0400

    Migrate to PostgreSQL for Development

--- Gemfile
+++ Gemfile
@@ -8,7 +8,8 @@ gem 'rails', '4.1.6'
 
 group :development, :test do
 	# Use sqlite3 as the database for Active Record
-	gem 'sqlite3'
+#	gem 'sqlite3'
+  gem 'pg'
 end
 group :production do
   gem 'pg'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140917222206) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "journals", force: true do |t|
     t.text     "journal_entry"
     t.datetime "created_at"
@@ -64,7 +67,7 @@ ActiveRecord::Schema.define(version: 20140917222206) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
