52b16b833743cd2f73f3de160f0e63cd291082b2
Adrian Cunanan <adrian@adriancunanan.com>
2014-10-27 16:49:53 -0400

    Self Care Data Entry Check for User Data

--- app/controllers/self_cares_controller.rb
+++ app/controllers/self_cares_controller.rb
@@ -49,8 +49,6 @@ class SelfCaresController < ApplicationController
     
     respond_to do |format|
       if @self_care.save
-        format.html { redirect_to self_cares_url, notice: 'Self Care Entry was successfully tracked.' }
-        format.json { render :show, status: :created, location: self_cares_url }
 
         # Create new Self Care object then write atributes to Parse
         parse_self_care = Parse::Object.new("SelfCare")
@@ -73,28 +71,45 @@ class SelfCaresController < ApplicationController
         parse_self_care["user_id"] = user["objectId"]
         parse_self_care.save
 
-        # Set UserData entry for Self Care Entry
+        # Find the beginning of the same day as Self Care Entry creation date
+        # and the beginning of the next day.  This is to be used to find
+        # dates in between... Meaning on the same day
+        date_check_begin = parse_self_care["createdAt"].to_date
+        date_check_end =  date_check_begin.tomorrow
+        date_check_begin = Parse::Date.new(date_check_begin)
+        date_check_end = Parse::Date.new(date_check_end)
+
+        # Set UserData entry for Sleep Entry
         user_data_query = Parse::Query.new("UserData").tap do |q|
           q.eq("UserID", parse_self_care["user_id"])
-          q.eq("createdAt".eql?(Parse::Date.new(parse_self_care["createdAt"]).to_date), true )
-          # q.eq(Date.parse("createdAt").to_s, Date.parse(parse_mood["createdAt"]).to_s)
-          # q.eq(("createdAt" - Parse::Date.new(@mood.timestamp)).abs, 0)
+          q.greater_than("createdAt", date_check_begin)
+          q.less_than("createdAt", date_check_end)
         end.get.first
 
         user_data = user_data_query
+
         if user_data == nil
           user_data = Parse::Object.new("UserData")
         end
-        user_data["SelfCare"] = parse_self_care.pointer
-        user_data["UserID"] = parse_self_care["user_id"]  
-        user_data.save
 
-        # Add UserData entry to User Entry
-        #if user["UserData"] == nil
-        #  user["UserData"] = Array.new
-        #end
-        #user["UserData"] << user_data.pointer
-        #user.save
+        if user_data["SelfCare"] == nil
+          user_data["SelfCare"] = parse_self_care.pointer
+          user_data["UserID"] = parse_self_care["user_id"]  
+          user_data.save
+
+          # Add UserData entry to User Entry
+          if user["UserData"] == nil
+            user["UserData"] = Array.new
+          end
+          user["UserData"] << user_data.pointer
+          user.save
+
+          format.html { redirect_to self_cares_url, notice: 'Self Entry was successfully tracked.' }
+          format.json { render :show, status: :created, location: sleeps_url }
+        else
+          @self_care.destroy
+          format.html { redirect_to self_cares_url, notice: 'Self Care Entry not created.  You already have one for this day.' }
+        end
 
       else
         format.html { render :new }
