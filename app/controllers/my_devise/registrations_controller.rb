a6d6dca61dba8a08dbf186e63f16f512fb7ad4dc
Adrian Cunanan <adrian@adriancunanan.com>
2014-11-09 12:37:21 -0500

    Resolve Parse User Update

--- app/controllers/my_devise/registrations_controller.rb
+++ app/controllers/my_devise/registrations_controller.rb
@@ -3,33 +3,37 @@ class MyDevise::RegistrationsController < Devise::RegistrationsController
 		super #this call Devise::RegistrationsController#create
 		# whenever a new user is created, create a new Parse User and
 		# populate the Parse User username and email with email that was just created
-		if resource.save
-			parse_user = Parse::User.new(
-	        {
-	          :first_name => rails_user_params[:firstName],
-	          :last_name => rails_user_params[:lastName],
-	          :username => rails_user_params[:email],
-	          :email => rails_user_params[:email],
-	          :password => rails_user_params[:password],
-	          :rails_user_id => current_rails_user.id.to_s
-	        }
-	    )
-	    parse_user.save
-	  end
+		if PARSE_ENABLED
+			if resource.save
+				parse_user = Parse::User.new(
+		        {
+		          :first_name => rails_user_params[:firstName],
+		          :last_name => rails_user_params[:lastName],
+		          :username => rails_user_params[:email],
+		          :email => rails_user_params[:email],
+		          :password => rails_user_params[:password],
+		          :rails_user_id => current_rails_user.id.to_s
+		        }
+		    )
+		    parse_user.save
+		  end
+		end
 	end
 
 	def update
-		user_to_update = current_rails_user
-		updated_first_name = rails_user_params[:firstName]
-		updated_last_name = rails_user_params[:lastName]
-		updated_email_username = rails_user_params[:email]
-		current_parse_user = Parse::Query.new("_User").eq("username", user_to_update.email).get.first
-		if current_parse_user != nil
-			current_parse_user["email"] = updated_email_username
-	    	current_parse_user["username"] = updated_email_username
-	    	current_parse_user["firstName"] = updated_first_name
-	    	current_parse_user["lastName"] = updated_last_name
-	    current_parse_user.save
+		if PARSE_ENABLED
+			user_to_update = current_rails_user
+			updated_first_name = rails_user_params[:firstName]
+			updated_last_name = rails_user_params[:lastName]
+			updated_email_username = rails_user_params[:email]
+			current_parse_user = Parse::Query.new("_User").eq("username", user_to_update.email).get.first
+			if current_parse_user != nil
+				current_parse_user["email"] = updated_email_username
+		    	current_parse_user["username"] = updated_email_username
+		    	current_parse_user["firstName"] = updated_first_name
+		    	current_parse_user["lastName"] = updated_last_name
+		    current_parse_user.save
+  		end
   	end
     super
   end
@@ -37,7 +41,7 @@ class MyDevise::RegistrationsController < Devise::RegistrationsController
 	def destroy
 		user_to_delete = current_rails_user
 		super
-		if resource.destroy
+		if resource.destroy && PARSE_ENABLED
 			current_parse_user = Parse::Query.new("_User").eq("email", user_to_delete.email).get.first
       current_parse_user.parse_delete
 		end
