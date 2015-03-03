07127e75c26cef087ccc4ec270e4dd69881badef
Adrian Cunanan <adrian@adriancunanan.com>
2015-03-03 17:27:57 -0500

    Update Paths to rails_users

--- config/routes.rb
+++ config/routes.rb
@@ -43,14 +43,14 @@ Rails.application.routes.draw do
   end
 
   devise_scope :rails_users do
-    match "/users/auth/:provider",
+    match "/rails_users/auth/:provider",
       constraints: { provider: /google|facebook/ },
-      to: "users/omniauth_callbacks#passthru",
+      to: "rails_users/omniauth_callbacks#passthru",
       as: :user_omniauth_authorize,
       via: [:get, :post]
-    match "/users/auth/:action/callback",
+    match "/rails_users/auth/:action/callback",
       constraints: { action: /google|facebook/ },
-      to: "users/omniauth_callbacks",
+      to: "rails_users/omniauth_callbacks",
       as: :user_omniauth_callback,
       via: [:get, :post]
   end
