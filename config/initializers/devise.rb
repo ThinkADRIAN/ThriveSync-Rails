3891a6ea9903cd819b58794db7b12171f1b8a6e9
Adrian Cunanan <adrian@adriancunanan.com>
2015-03-05 12:59:49 -0500

    Use Environment Variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,7 +257,7 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  config.omniauth :facebook, "659380040766459", "ba694b8ada2b057f6ace227b3aa846f6"
+  config.omniauth :facebook, ENV['FB_ID'], ENV['FB_SECRET']
   #config.omniauth :twitter, "KHTNRYmtLJB2XMnXfFctXxxit", "WFAKxj34Fxi4VFiKDUlOxeOMUOVwghGMJ9fkhYTCULTCPKlx4w"
 
   require 'omniauth-google-oauth2'
