e822362cb2c84f2255398c6a046b7fd07dae5d32
Adrian Cunanan <adrian@adriancunanan.com>
2015-03-16 17:50:28 -0400

    Merge branch 'master' of heroku.com:thrivetracker

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,6 +5,25 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
         @rails_user = RailsUser.find_for_oauth(env["omniauth.auth"], current_rails_user)
 
         if @rails_user.persisted?
+
+          # Create Parse User As necessary
+          if $PARSE_ENABLED
+            current_parse_user = Parse::Query.new("_User").eq("username", @rails_user[:email]).get.first
+            if current_parse_user.blank?
+              parse_user = Parse::User.new(
+                  {
+                    :firstName => @rails_user[:first_name],
+                    :lastName => @rails_user[:last_name],
+                    :username => @rails_user[:email],
+                    :email => @rails_user[:email],
+                    :password => @rails_user[:password].to_s,
+                    :rails_user_id => @rails_user.id.to_s
+                  }
+              )
+              parse_user.save
+            end
+		      end
+
           sign_in_and_redirect @rails_user, event: :authentication
           set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
         else
