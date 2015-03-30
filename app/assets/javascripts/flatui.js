360d12a044839220080ae2f581a3d4aef242dbf4
Adrian Cunanan <adrian@adriancunanan.com>
2015-03-30 20:00:13 +0000

    Resolve JS File Location Issue for FlatUI

--- app/assets/javascripts/flatui.js
+++ app/assets/javascripts/flatui.js
@@ -1,7 +1,7 @@
-//= require jquery.ui.touch-punch.min
-//= require bootstrap-select
-//= require bootstrap-switch
-//= require flatui-checkbox
-//= require flatui-radio
-//= require jquery.tagsinput
-//= require jquery.placeholder
+//= require ../startup-framework/flat-ui/js/jquery.ui.touch-punch.min
+//= require ../startup-framework/flat-ui/js/bootstrap-select
+//= require ../startup-framework/flat-ui/js/bootstrap-switch
+//= require ../startup-framework/flat-ui/js/flatui-checkbox
+//= require ../startup-framework/flat-ui/js/flatui-radio
+//= require ../startup-framework/flat-ui/js/jquery.tagsinput
+//= require ../startup-framework/flat-ui/js/jquery.placeholder
