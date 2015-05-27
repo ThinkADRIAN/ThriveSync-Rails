ed86dfbf7809ae05889caf11890c852d4d092c3c
Adrian Cunanan <adrian@adriancunanan.com>
2015-05-27 09:58:49 -0400

    Configure Simple Token Authentication

--- app/models/rails_user.rb
+++ app/models/rails_user.rb
@@ -26,6 +26,8 @@ class RailsUser < ActiveRecord::Base
   TEMP_EMAIL_PREFIX = 'change@me'
   TEMP_EMAIL_REGEX = /\Achange@me/
 
+  acts_as_token_authenticatable
+
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable
   devise :database_authenticatable, :registerable,
