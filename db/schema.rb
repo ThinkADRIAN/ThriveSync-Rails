58d035a87a62e29a96f9d886260f7476b88ef1fb
Adrian Cunanan <adrian@adriancunanan.com>
2015-06-04 16:52:36 -0400

    Update Schema for Reminders Class

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150604204445) do
+ActiveRecord::Schema.define(version: 20150604205136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,15 @@ ActiveRecord::Schema.define(version: 20150604204445) do
     t.integer  "user_id"
     t.integer  "relation_id"
     t.string   "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reminders", force: true do |t|
+    t.integer  "user_id"
+    t.string   "message"
+    t.string   "day_mask"
+    t.datetime "alert_time"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
