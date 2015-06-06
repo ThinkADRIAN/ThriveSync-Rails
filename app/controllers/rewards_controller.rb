ea04136ed7c7652b91bcaf28d425dd7184a7de5f
Adrian Cunanan <adrian@adriancunanan.com>
2015-06-06 15:31:42 -0400

    Secure Rewards Controller

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,37 +1,66 @@
 class RewardsController < ApplicationController
-  before_action :set_reward, only: [:show, :edit, :update, :destroy]
+  acts_as_token_authentication_handler_for User
 
-  respond_to :html
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
+  #load_and_authorize_resource
+  check_authorization
+
+  before_action :set_reward, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
+  respond_to :html, :js, :json, :xml
+
+  Time.zone = 'EST'
 
   def index
-    @rewards = Reward.all
-    respond_with(@rewards)
+    authorize! :manage, Reward
+    @user = User.find_by_id(params[:user_id])
+
+    if @user == nil
+      @reward = Reward.where(user_id: current_user.id)
+    elsif @user != nil
+      @reward = Reward.where(user_id: @user.id)
+    end
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @reward, status: 200 }
+    end
   end
 
   def show
+    authorize! :manage, Reward
     respond_with(@reward)
   end
 
   def new
+    authorize! :manage, Reward
     @reward = Reward.new
     respond_with(@reward)
   end
 
   def edit
+    authorize! :manage, Reward
   end
 
   def create
+    authorize! :manage, Reward
     @reward = Reward.new(reward_params)
     @reward.save
     respond_with(@reward)
   end
 
   def update
+    authorize! :manage, Reward
     @reward.update(reward_params)
     respond_with(@reward)
   end
 
   def destroy
+    authorize! :manage, Reward
     @reward.destroy
     respond_with(@reward)
   end
