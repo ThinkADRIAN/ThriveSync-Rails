7b73c5ab463bb0626b9be35eb44431bd17588d48
Adrian Cunanan <adrian@adriancunanan.com>
2015-06-06 14:47:32 -0400

    Initial Configuration of Rewards Model and User Association

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,2 +1,26 @@
 class Reward < ActiveRecord::Base
+	belongs_to :user
+
+  after_initialize :init
+
+  def init
+    self.completed_first_entry ||= false
+    self.completed_first_streak ||= false
+    self.rewards_enabled ||= true
+  end
+
+  def self.mark_first_entry_completed
+    self.completed_first_entry = true
+    self.save
+  end
+
+  def self.mark_first_streak_completed
+    self.completed_first_streak = true
+    self.save
+  end
+
+  def self.set_rewards_enabled(bool_value)
+    self.rewards_enabled = bool_value
+    self.save
+  end
 end
