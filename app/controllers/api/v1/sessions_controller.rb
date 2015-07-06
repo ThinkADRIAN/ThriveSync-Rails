76b33d64da09e759e2c27a1c0ab99bba38d42d59
Adrian Cunanan <adrian@adriancunanan.com>
2015-07-06 12:01:44 -0400

    Track User Logged In for Segment.io

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -12,7 +12,20 @@ class Api::V1::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   def create
-    super
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_flashing_format?
+    sign_in(resource_name, resource)
+
+    # Track User Logged In for Segment.io Analytics
+    Analytics.track(
+      user_id: resource.id,
+      event: 'Logged In',
+      properties: {
+      }
+    )
+
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
   end
 
 
