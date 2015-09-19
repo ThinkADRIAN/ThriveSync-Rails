b2a73ad181598b0093934485fb5aeeb31c960c39
Adrian Cunanan <adrian@adriancunanan.com>
2015-09-19 16:57:44 -0400

    Limit Sending Cards to Friends

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,9 +1,19 @@
 module MessagesHelper
   def recipients_options
-    s = ''
-    User.all.each do |user|
-      s << "<option value='#{user.id}'>#{user.name}</option>"
-    end
-    s.html_safe
+  	@friends = current_user.friends
+    @pending_friends = current_user.pending_invited_by
+    @unconfirmed_friends = current_user.pending_invited
+    @confirmed_friends = @friends - ( @pending_friends | @unconfirmed_friends)
+    @users = User.where.not(id: current_user.id)
+  	
+  	if !@confirmed_friends.empty?
+  		for user in @friends
+  			if current_user.friend_with? user
+          s = ''
+	      	s << "<option value='#{user.id}'>#{user.first_name} #{user.last_name}</option>"
+	      end
+	    end
+	    s.html_safe
+	  end
   end
 end
