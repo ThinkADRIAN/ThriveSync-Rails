eaf479681203d2b997070719b84b5ebe53822505
Adrian Cunanan <adrian@adriancunanan.com>
2015-09-23 12:10:46 -0400

    Configure Sending Cards from Supporters to Thrivers

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,23 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_thriver, only: [:new, :create]
 
   def new
   	c = PreDefinedCard.all
-	random_ids = c.ids.sort_by { rand }.slice(0, 3)
-	@random_cards = PreDefinedCard.where(:id => random_ids)
+  	random_ids = c.ids.sort_by { rand }.slice(0, 3)
+  	@random_cards = PreDefinedCard.where(:id => random_ids)
   end
 
   def create
-    recipients = User.where(id: params['recipients'])
+    recipients = User.where(id: @thriver.id)
     conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
     flash[:success] = "Message has been sent!"
     redirect_to conversations_path#conversation_path(conversation)
+  end
+
+  private
+
+  def set_thriver
+    @thriver = User.find(params[:thriver_id])
   end
 end
--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,33 +1,15 @@
 module MessagesHelper
   def recipients_options
     @friends = current_user.friends
-    @pending_friends = current_user.pending_friends
-    @requested_friends = current_user.requested_friends
-    @users = User.where.not(id: current_user.id)
   	
   	if !@friends.empty?
   		for user in @friends
-  			if current_user.friend_with? user
+  			if @thriver.supporters.include? current_user.id
           s = ''
-	      	s << "<option value='#{user.id}'>#{user.first_name} #{user.last_name}</option>"
+	      	s << "<option value='#{@thriver.id}'>#{@thriver.first_name} #{@thriver.last_name}</option>"
 	      end
 	    end
 	    s.html_safe
 	  end
   end
 end
-
-# Send to Clients
-=begin
-  	@users = User.where(:email == !nil).sort_by { |r| r.last_name.downcase }
-  	
-  	if !@users.empty?
-	    s = ''
-	    @users.each do |user|
-	    	if current_user.clients.include? user.id
-	      	s << "<option value='#{user.id}'>#{user.name}</option>"
-	      end
-	    end
-	    s.html_safe
-	  end
-=end
