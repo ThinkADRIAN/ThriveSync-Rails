5be1630005472f31d402e2dec63b17a688384c30
Adrian Cunanan <adrian@adriancunanan.com>
2015-09-24 06:57:01 -0400

    Handle support invitations for existing users

--- app/controllers/my_devise/invitations_controller.rb
+++ app/controllers/my_devise/invitations_controller.rb
@@ -7,7 +7,36 @@ class MyDevise::InvitationsController < Devise::InvitationsController
 
   # POST /resource/invitation
   def create
-    super
+    invitee = User.find_by_email(params[:user][:email])
+
+    # Handle if invitee account already exists and is not already an existing supporter
+    if invitee != nil && !(invitee.is? :pro) && !(current_user.supporters.include? invitee.id.to_i)
+      current_user.supporters += [invitee.id.to_i]
+      current_user.save!
+      current_user.friend_request(invitee)
+      invitee.roles += ["supporter"]
+      invitee.save!
+
+      respond_to do |format|
+        format.html { redirect_to after_invite_path_for(current_user), :flash => { :success => "Supporter Invititation sent to #{(params[:user][:email])}" } }
+        format.json { render :json => @cards, status: 200 }
+      end
+    # Handle if invitee account already exists and is already an existing supporter
+    elsif invitee != nil && !(invitee.is? :pro) && (current_user.supporters.include? invitee.id.to_i)
+      respond_to do |format|
+        format.html { redirect_to new_user_invitation_path, :flash => { :error => "This email is associated to an existing supporter." }}
+        format.json { render :error => "This email is associated to an existing supporter." }
+      end
+    # Handle if invitee account is a pro
+    elsif invitee != nil && (invitee.is? :pro)
+      respond_to do |format|
+        format.html { redirect_to :back, :flash => { :error => "Mental Health Providers cannot be invited as a peer supporter." }}
+        format.json { render :error => "Mental Health Providers cannot be invited as a peer supporter." }
+      end
+    # Default invitation for new users
+    else
+      super
+    end
   end
 
   # GET /resource/invitation/accept?invitation_token=abcdef
@@ -24,7 +53,7 @@ class MyDevise::InvitationsController < Devise::InvitationsController
       inviter = current_user.invited_by
       inviter.supporters += [current_user.id.to_i]
       inviter.save!
-      # Add Supporter Role to user and rmeove User Role
+      # Add Supporter Role to user and remove User Role
       current_user.roles += ["supporter"]
       current_user.roles -= ["user"]
       current_user.save!
