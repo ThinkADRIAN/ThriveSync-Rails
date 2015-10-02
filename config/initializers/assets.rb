aa2974087b0aed5a85c25055369ce64f780b9078
Adrian Cunanan <adrian@adriancunanan.com>
2015-10-02 19:09:16 -0400

    Require Tree for Bootstrap Material Design

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( welcome.css */bootstrap-material-design/* */startup-framework/* )
+Rails.application.config.assets.precompile += %w( welcome.css )
