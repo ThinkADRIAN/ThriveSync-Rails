eb5404987cb05feb99456082a94dad2ff872f84a
Adrian Cunanan <adrian@adriancunanan.com>
2015-10-20 14:57:36 -0400

    Configure Rspec Tests for PreDefinedCards

--- spec/factories/pre_defined_cards.rb
+++ spec/factories/pre_defined_cards.rb
@@ -1,7 +1,8 @@
-FactoryGirl.define do
-  factory :pre_defined_card do
-    text "MyString"
-category "MyString"
-  end
+require 'faker'
 
+FactoryGirl.define do
+  factory :pre_defined_card do |f|
+    f.text { Faker::Lorem.sentence }
+    f.category { Faker::Lorem.word }
+  end
 end
