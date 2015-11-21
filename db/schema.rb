a21fcb0b1da82a342fa81c50c3556157f178b1de
Adrian Cunanan <adrian@adriancunanan.com>
2015-11-21 14:33:51 -0500

    Add UUID to User Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120154348) do
+ActiveRecord::Schema.define(version: 20151121190332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -301,6 +301,7 @@ ActiveRecord::Schema.define(version: 20151120154348) do
     t.integer  "supporters",             default: [],                                        array: true
     t.datetime "research_started_at"
     t.integer  "thrivers",               default: [],                                        array: true
+    t.string   "uuid"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", using: :btree
