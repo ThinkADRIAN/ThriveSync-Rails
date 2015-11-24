ecc4251888b78db50f73132a34d22c6555a6beae
Adrian Cunanan <adrian@adriancunanan.com>
2015-11-24 09:12:33 -0500

    Validate Uniqueness of Stripe ID

--- app/models/subscription_plan.rb
+++ app/models/subscription_plan.rb
@@ -1,6 +1,8 @@
 class SubscriptionPlan < ActiveRecord::Base
   include Payola::Plan
 
+  validates_uniqueness_of :stripe_id
+
   def redirect_path(subscription)
     # you can return any path here, possibly referencing the given subscription
     '/'
