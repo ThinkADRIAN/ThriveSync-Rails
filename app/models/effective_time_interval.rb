0d4b2abca767826662871b15dbba02d07a2e449c
Adrian Cunanan <adrian@adriancunanan.com>
2016-02-15 09:57:28 -0500

    Setup Effective Time Interval Model, Relationship to Passive Data Point, and Nested Attribute Access

--- app/models/effective_time_interval.rb
+++ app/models/effective_time_interval.rb
@@ -1,2 +1,5 @@
 class EffectiveTimeInterval < ActiveRecord::Base
+  belongs_to :passive_data_point
+
+  validates_presence_of :passive_data_point_id, :start_date_time, :end_date_time
 end
--- app/models/passive_data_point.rb
+++ app/models/passive_data_point.rb
@@ -1,5 +1,8 @@
 class PassiveDataPoint < ActiveRecord::Base
   belongs_to :user
+  has_many :effective_time_intervals
+
+  accepts_nested_attributes_for :effective_time_intervals
 
   validates_presence_of :source_uuid, :creation_date_time, :schema_namespace, :schema_name, :schema_version, :effective_date_time
 end
