207a90619475326df80aebae7d1ae25809c4ad25
Adrian Cunanan <adrian@adriancunanan.com>
2016-02-15 18:04:10 -0500

    Add Effective Time Intervals to Passive Data Point JSON Outputs

--- app/serializers/passive_data_point_serializer.rb
+++ app/serializers/passive_data_point_serializer.rb
@@ -1,3 +1,5 @@
 class PassiveDataPointSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :was_user_entered, :timezone, :source_uuid, :external_uuid, :creation_date_time, :schema_namespace, :schema_name, :schema_version, :effective_date_time
+
+  has_many :effective_time_intervals, include_nested_associations: true
 end
