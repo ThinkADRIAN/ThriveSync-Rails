118a044d6bbc4a2d07d08dc1fcd5458cf8b00d40
Adrian Cunanan <adrian@adriancunanan.com>
2016-02-16 12:00:23 -0500

    Setup Relationships, Nested Attributes, and Validations for Passive Workout

--- app/models/passive_workout.rb
+++ app/models/passive_workout.rb
@@ -1,2 +1,7 @@
 class PassiveWorkout < ActiveRecord::Base
+  belongs_to :passive_data_point
+
+  validates :kcal_burned_value, numericality: {only_float: true, greater_than_or_equal_to: 0}
+  validates :distance_value, numericality: {only_float: true, greater_than_or_equal_to: 0}
+  validates_presence_of :workout_type
 end
