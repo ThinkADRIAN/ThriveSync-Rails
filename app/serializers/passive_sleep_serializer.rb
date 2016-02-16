ce99e42d3f18100b11bcb0c3a3efd72f327b2245
Adrian Cunanan <adrian@adriancunanan.com>
2016-02-15 21:03:59 -0500

    Adjust Passive Data Point Id References in Serializer

--- app/serializers/passive_sleep_serializer.rb
+++ app/serializers/passive_sleep_serializer.rb
@@ -1,3 +1,3 @@
 class PassiveSleepSerializer < ActiveModel::Serializer
-  attributes :id, :passive_data_id, :category_type, :category_value, :value, :unit
+  attributes :id, :passive_data_point_id, :category_type, :category_value, :value, :unit
 end
