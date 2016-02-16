d5b833fd1f348f9e88440e9ada4bd65c8391e2d3
Adrian Cunanan <adrian@adriancunanan.com>
2016-02-16 16:05:58 -0500

    Updated Migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216165711) do
+ActiveRecord::Schema.define(version: 20160216190336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
