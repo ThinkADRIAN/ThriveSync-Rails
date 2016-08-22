76e430458011ec8e07a210d836ab5077513513d2
Adrian Cunanan <adrian@adriancunanan.com>
2016-08-21 20:06:12 -0400

    Update Let's Encrypt Verification Code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def letsencrypt
     # http://collectiveidea.com/blog/archives/2016/01/12/lets-encrypt-with-a-rails-app-on-heroku/
     # use your code here, not mine
-    render text: "um3keHHyMrzs3rT8c6H67Ms0biTGGQgfqh7O0A3a988.FWL9KYn1l5bu2Njcv4UdK6U0haCZGGtz7HEnmmxBhHk"
+    render text: "VZm19jETsVMB69LDv3MCbeHtn7k6LrCqn9RuBJYrByQ.FWL9KYn1l5bu2Njcv4UdK6U0haCZGGtz7HEnmmxBhHk"
   end
 end
