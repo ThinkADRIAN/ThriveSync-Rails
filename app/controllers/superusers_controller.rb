5ed3852d2f13c4f0020f97b5c1b9e6713bd20e60
Adrian Cunanan <adrian@adriancunanan.com>
2016-08-29 21:01:28 -0400

    Configure CSV Export Functionality

--- app/controllers/superusers_controller.rb
+++ app/controllers/superusers_controller.rb
@@ -1,3 +1,48 @@
 class SuperusersController < ApplicationController
   before_action :authenticate_user!
+
+  def export_moods_csv
+    user_email = params[:email]
+    @user = User.where(email: user_email).first
+    @moods = Mood.where(user_id: @user.id)
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @moods.to_csv }
+    end
+  end
+
+  def export_sleeps_csv
+    user_email = params[:email]
+    @user = User.where(email: user_email).first
+    @sleeps = Sleep.where(user_id: @user.id)
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @sleeps.to_csv }
+    end
+  end
+
+  def export_self_cares_csv
+    user_email = params[:email]
+    @user = User.where(email: user_email).first
+    @self_cares = SelfCare.where(user_id: @user.id)
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @self_cares.to_csv }
+    end
+  end
+
+  def export_journals_csv
+    user_email = params[:email]
+    @user = User.where(email: user_email).first
+    @journals = Journal.where(user_id: @user.id)
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @journals.to_csv }
+    end
+  end
+
 end
--- app/models/journal.rb
+++ app/models/journal.rb
@@ -2,4 +2,13 @@ class Journal < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :journal_entry, :timestamp
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |journal|
+        csv << journal.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
